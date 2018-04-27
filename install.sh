96ffcb494af7bcb8945923aabea1d0a26cde5d35
vtejapy <34569554+vtejapy@users.noreply.github.com>
2018-04-27 17:58:55 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo yum install epel-release
+sudo yum install java-1.8.0-openjdk.x86_64
+sudo cp /etc/profile /etc/profile_backup
+echo 'export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk' | sudo tee -a /etc/profile
+echo 'export JRE_HOME=/usr/lib/jvm/jre' | sudo tee -a /etc/profile
+source /etc/profile
+cd ~
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install jenkins
+sudo systemctl start jenkins.service
+sudo systemctl enable jenkins.service
